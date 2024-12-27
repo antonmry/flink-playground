5804d99eefd70bbed4373e6c17f5bd1f0af39675
Anton Rodriguez <anton@galiglobal.com>
2024-12-27 18:07:49 +0100

    AsyncState working locally and S3

--- state-backends/src/main/java/com/galiglobal/DataStreamJob.java
+++ state-backends/src/main/java/com/galiglobal/DataStreamJob.java
@@ -2,7 +2,6 @@ package com.galiglobal;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.configuration.CheckpointingOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.StateBackendOptions;
 import org.apache.flink.connector.file.table.FileSystemConnectorOptions;
@@ -10,7 +9,6 @@ import org.apache.flink.state.forst.ForStOptions;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.Table;
@@ -21,7 +19,7 @@ import org.apache.flink.util.Collector;
 
 import java.time.Duration;
 
-import org.apache.flink.state.forst.ForStConfigurableOptions;
+import static org.apache.flink.runtime.state.StateBackendLoader.FORST_STATE_BACKEND_NAME;
 
 public class DataStreamJob {
 
@@ -31,16 +29,16 @@ public class DataStreamJob {
         if (args.length > 0 && "test".equals(args[0])) {
 
             Configuration conf = new Configuration();
-            //conf.setString("state.backend.type", "hashmap");
-            //conf.set(StateBackendOptions.STATE_BACKEND, "rocksdb");
-            conf.set(StateBackendOptions.STATE_BACKEND, "forst");
-            conf.set(ForStOptions.REMOTE_DIRECTORY, "s3://arodriguez-flink-poc/flink/db");
-            conf.set(ForStOptions.REMOTE_DIRECTORY, "s3://arodriguez-flink-poc/flink/db");
+            // conf.setString("state.backend.type", "hashmap");
+            // conf.set(StateBackendOptions.STATE_BACKEND, "rocksdb");
+            conf.set(StateBackendOptions.STATE_BACKEND, FORST_STATE_BACKEND_NAME);
+            // conf.set(ForStOptions.REMOTE_DIRECTORY, "s3://arodriguez-flink-poc/flink/db");
+            conf.set(ForStOptions.REMOTE_DIRECTORY, "file:///tmp/flink/remote");
 
-            conf.set(CheckpointingOptions.CHECKPOINT_STORAGE, "filesystem");
-           //conf.set(CheckpointingOptions.CHECKPOINTS_DIRECTORY, "file:///tmp/flink/checkpoint");
-           conf.set(CheckpointingOptions.CHECKPOINTS_DIRECTORY, "s3://arodriguez-flink-poc/flink/checkpoint");
-            conf.set(CheckpointingOptions.CHECKPOINTING_INTERVAL, Duration.ofSeconds(5));
+            // conf.set(CheckpointingOptions.CHECKPOINT_STORAGE, "filesystem");
+            // conf.set(CheckpointingOptions.CHECKPOINTS_DIRECTORY, "file:///tmp/flink/checkpoint");
+            // conf.set(CheckpointingOptions.CHECKPOINTS_DIRECTORY, "s3://arodriguez-flink-poc/flink/checkpoint");
+            //  conf.set(CheckpointingOptions.CHECKPOINTING_INTERVAL, Duration.ofSeconds(5));
 
             env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
         } else {
@@ -72,11 +70,11 @@ public class DataStreamJob {
         KeyedStream<Tuple2<String, Integer>, String> keyedStream = resultStream.flatMap(new Splitter())
             .keyBy(value -> value.f0);
 
-        // Not available yet in 2.0-preview1
-        // keyedStream.enableAsyncState();
+        // Not available yet in 2.0-preview1, only 2.0-SNAPSHOT
+        keyedStream.enableAsyncState();
 
         keyedStream
-            .window(TumblingProcessingTimeWindows.of(Duration.ofSeconds(5)))
+            //  .window(TumblingProcessingTimeWindows.of(Duration.ofSeconds(5)))
             .sum(1)
             .print();
 
